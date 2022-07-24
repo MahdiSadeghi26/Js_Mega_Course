d04187b4e1014651ca904f675013d46758c74d3a
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2022-07-24 11:18:59 +0430

    data id added & getCourseInfo fnc added!

--- js/app.js
+++ js/app.js
@@ -1,21 +1,39 @@
 // ** variables **
 // container containes courses
-const courses=document.querySelector('.container')
+const courses = document.querySelector(".container");
 
 // ** event Listeners **
-eventListeners()
+eventListeners();
 function eventListeners() {
-    courses.addEventListener('click',buyCourse)
+  // bu
+  courses.addEventListener("click", buyCourse);
 }
 
 //** functions **
+// detecting course from buy Button
 function buyCourse(e) {
-    e.preventDefault()
-    if (e.target.classList.contains('buyButton')) {
-        const course=e.target.parentElement.parentElement
-        getCourseInfo(course)
-    }
+  e.preventDefault();
+  //selecting buy Button and parent element
+  if (e.target.classList.contains("buyButton")) {
+    const course = e.target.parentElement.parentElement;
+    // sending course to getCourseInfo function to getting information
+    getCourseInfo(course);
+  }
 }
+// getting course Info selected buy user
 function getCourseInfo(course) {
-    console.log(course)
+  console.log(course);
+  // creating obj and imooring course info in obj
+  const courseInfo={
+    image:course.querySelector('img'),
+    title:course.querySelector('h4').textContent,
+    price:course.querySelector('.newPrice').textContent,
+    id:course.querySelectorAll('a')[1].getAttribute('date-id')
+  }
+  // adding course to the cart
+  console.log(courseInfo);
+}
+// course info funciton
+function courseInfo() {
+  
 }
